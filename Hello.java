41d7793b30d67f29c42c7018d3a2bf5883ca492f
Eduardo Bertolin <151974140+Bertol4@users.noreply.github.com>
2025-02-27 23:04:27 -0300

    Adicionando Hola que tal

--- Hello.java
+++ Hello.java
@@ -1,9 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         helloWorld();
+        olaQueTal();
     }
 
     public static void helloWorld() {
         System.out.println("Hello World");
     }
+
+    public static void holaQueTal() {
+    System.out.println("Hola que tal");
+}
+
 }
